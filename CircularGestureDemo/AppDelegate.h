c1726d34e36f0a3fdabc4142399d7939cd52e4b6
Thomas Mellenthin <mellenthin@teufel.de>
2012-01-29 14:41:45 +0100

    Initial Commit

---
+++ CircularGestureDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CircularGestureDemo
+//
+//  Created by Thomas Mellenthin (Raumfeld) on 1/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
